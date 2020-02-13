a950249640864700311a21d37717d571f0027ed4
infoTercen <info@tercen.com>
2020-02-13 16:33:23 +0000

    init repo

---
+++ doc/DEPLOY.md
@@ -0,0 +1,13 @@
+## Deployment lines
+
+```R
+renv::init()
+```
+
+#!/usr/bin/env bash
+
+```
+git add -A && git commit -m "init repo"
+git push
+git tag -a 0.0.1 -m "++" && git push --tags
+```
---
+++ workspace.R
@@ -0,0 +1,19 @@
+library(tercen)
+library(dplyr)
+
+options("tercen.serviceUri" = "https://beta.tercen.com/api/v1")
+options("tercen.username" =   "xxxx")
+options("tercen.password" =   "yyyy")
+
+options("tercen.workflowId" = "wwww")
+options("tercen.stepId"     = "dddd")
+
+getOption("tercen.workflowId")
+getOption("tercen.stepId")
+
+(ctx = tercenCtx())  %>% 
+  select(.y, .ci, .ri) %>% 
+  group_by(.ci, .ri) %>%
+  summarise(median = median(.y)) %>%
+  ctx$addNamespace() %>%
+  ctx$save()
